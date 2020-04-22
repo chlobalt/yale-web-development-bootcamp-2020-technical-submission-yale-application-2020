e9dc0d173dda7629f9c66935b8dbdcd0eba5635f
Chloe Hong <chloe.hong@yale.edu>
2020-04-22 09:26:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
+name = "Chloe"
+age = 20
+favorite_food = "fried chicken"
+height_in_inches = 62
 
+AVERAGE_HEIGHT_IN_INCHES = 
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
